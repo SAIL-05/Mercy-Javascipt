ca3ca01c4e294c7d369e1286f8c6b5bb1be9f76e
Gorgeous568 <iregbeyenm@gmail.com>
2025-05-05 21:19:14 +0100

    script

---
+++ script.js
@@ -0,0 +1,182 @@
+// console.log(200)
+
+// let myAge = 34
+// console.log(myAge);
+
+ // strings '' ""
+
+// let keziah= "today is keziah's birthday"
+// console.log(keziah)
+//number
+
+
+// // let myNum = 11
+// // console.log(myNum)
+// //boolean 
+
+// // //undefined
+// // let test;
+// // console.log(test);
+
+// ///null 
+// // const test2 = null
+
+// // Non-primitive data types
+//  // objects
+
+// // const student = {
+// // name: "keziah",
+// // age: 21,
+// // ispregnant:true
+
+// // }
+// // console.log(student)
+
+//  // arrays
+
+// // const myArray = ["Amaka", "James",543,
+// // true, {name: "John"}, [2,4,6]]
+// // console.log(myArray[2]);
+
+
+
+
+//  // functions
+
+
+// // Arithemetic Operator
+
+// // let a = 34
+// // let b = 56
+
+// // Addition
+// // const sum = a+b;
+// // console.log(sum);
+
+// // Multiply
+// // const mut = a*b;
+// // console.log(mut);
+
+//  // Division
+// // const div = a/b;
+// // console.log(div);
+
+// // Modulus
+// // const modul = b % a;
+// // console.log(modul)
+
+//  // Assignments Operators
+// // let c =39
+//  // +=
+//  // -=
+
+//  // Comparism Operators
+// // const d ="Jane";
+// // const e = "Jane";
+// // const f = "Mary";
+// // const g = 23;
+// // const h = 47;
+// // const i = 23;
+
+// // Equals To
+// // console.log(d==e);
+
+//  //Greater than
+
+// // console.log (e > h)
+// // Less than
+// // console.log (g < h)
+//  // Greater than equals to
+// // console.log (g >= i);
+// // Less than equals to
+// // console.log(g)
+
+// // console.log
+
+
+// //Logical Operator
+// // Logical OR  ||
+//  // Logical NOT !
+//  // Logical AND &&
+
+// // Ayo's Admission Details
+// const fee = true;
+// const uniform = false;
+// const textbooks = false;
+// const idcard = false;
+
+// const checkOR = fee|| textbooks || uniform ||idcard;
+// const checkAND = fee && textbooks && uniform && idcard;
+// const checkAyo = fee && textbooks;
+// console.log(checkAyo) = (fee && idcard ) || (fee && uniform);
+// console.log(checkAyoAgain)
+// console.log
+
+// //Urnary Operator
+// // Increment ++
+// // Decrement --
+
+// let k = 3;
+// k++;
+// console.log(k)
+
+// k--;
+// console.log(k)
+
+// // Tenary Operator
+// // (condition)? "" : ""
+
+// const tenTest = 76 > 45 ? "Yes This is correct" :
+// "No, That is wrong"
+
+// console.log(tenTest)
+
+// let age = 18
+
+// let voting = age <18 ? "You cannot vote" : "You can vote"
+// console.log(voting)
+// let voting = Age =65
+
+// // Conditionals
+// //  if statement
+// //  if else statement
+// //  if else if statement
+
+// //  if(){}
+
+// // if condition
+// // if (a > b) { =
+// console.log(The first value is the greatest)}
+
+// // prompt()
+// prompt ("Enter your age")
+// const myAge =prompt ("Enter your age")
+// console.log (myAge)
+// alert (myAge)
+// const test = 78;
+// console.log (`Your lucky number is ${test}`);
+
+// // let ValueOne = prompt ("Enter your first Number");
+// // let valueTwo = prompt ("Enter your second Number")
+// // let ans = valueOne + valueTwo;
+// // alert(ans);
+
+// // Exercise list
+// 1. 
+
+// let number = -18
+// if (number >0) {
+//     console.log (""
+
+    
+
+
+2.
+Let Value one = 18
+Let Value two = 25
+
+
+// let ValueOne = prompt(Even number)
+// let ValueOne = prompt(Odd number)
+
+
