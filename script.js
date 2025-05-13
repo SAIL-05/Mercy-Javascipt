75521a1074d8d7ede67dc2ffa43a001bfe3a0c82
Gorgeous568 <iregbeyenm@gmail.com>
2025-05-13 06:58:20 +0100

    new update

--- script.js
+++ script.js
@@ -219,32 +219,114 @@
 // else{ alert("score is F");}
 
 
-let BMI =parseFloat()prompt("")
-console.log(bmi)
-// let score =parseInt
 
-   
-// let score=parseInt(prompt("display grade"));
+ 
+// Exercise
+// Write a program that calculates the ticket price based on age with the following conditions: age below 12 pay a ticket price of 5, age below 18 pay a ticket price of 10, age below 60 pay a ticket price of 20, age over 60 play a ticket price of 15.
 
-//  if(score>75 && score < 100) { 
-//     alert("score is ");}
-// else if(score>69 && score < 100) {
-//      alert("score is B");}
-// else if(score>59 && score < 67) {
-//      alert("score is C");}
-// else if(score>49 && <58) { 
-//     alert("score is D");}
-// else if(score>44-48) {
-//      alert("score is E");}
-// else{ alert("score is F");}
-// else if(score>49 && <58) { 
-//     alert("score is D");}
-// else if(score>44-48) {
-//      alert("score is E");}
-// else{ alert("score is F");}
-// else if(score>49 && <58) { 
-//     alert("score is D");}
-// else if(score>44-48) {
-//      alert("score is E");}
-// else{ alert("score is F");}
+// Write a program that determines if a year is a leap year.
+
+// Write a program that calculates a discount based on the purchase amount.Prices equal or over 100 discount have a discount of 20. Prices equal or over 50 have a discount of 10. Otherwise discount is 0
+
+// Write a program that greets the user based on the time of day. Display good morning, good afternnon or good evening based on the time of day when you run the code.
+
+// Write a program that calculates the Body Mass Index (BMI) and categorizes it. The formula for BMI is: weight / (height * height).
+// Underweight (below 18.5), Healthy Weight (18.5 to 24.9), Overweight (25 to 29.9), and Obese (30 or greater)
+
+// Write a simple number guessing game. Provide a secret number and a guess. Based on those numbers give players clues if their guess is higher, lower or correct.
+
+
+// let weight = parseFloat(prompt("enter weight in kg"))
+// let height = parseFloat(prompt("enter height in meter"))
+
+// let bmi = weight / (height * height)
+//  if (bmi <= 18.5){
+//     alert("lepa shandi")
+// } 
+// else if (bmi >18.5 || bmi < 24.9){
+//     alert ("perfect shape")
+// }
+// else if (bmi >25 || bmi < 29.9){
+//     alert ("you're adding up")
+// }
+// else if (bmi>30){
+//     alert("Fat")
+// }
+// else{
+//     alert("input accurate weight")
+// }
+
+
+
+
+// guessing game
+// let number = parseInt(prompt("enter your guess"))
+
+// if (number === 52){
+//     alert(`${number} is correct `)
+// }
+// else if (number < 52){
+//     alert("you guess is lower")
+// }
+// else if (number > 52){
+//     alert("your guess is higher")
+// }
+// else {
+//     alert('try again')
+// }
+ 
+
+
+// let amount = parseInt(prompt("amount"))
+// if (amount >= 100){
+//     alert (`${amount}` * 0.2)
+// }
+// else if (amount >= 50){
+//     alert (`${amount}` * 0.1)
+// }
+// else {
+//     alert( "discount is 0")
+// }
+
+
+
+
+// let year = parseInt(prompt("enter year"))
+// if (year % 4 ===0 && year % 100 !== 0 || year % 400 === 0){
+//     alert(`${year} is a leap year`)
+// }else {
+//     alert(`${year} is not a leap year`)
+// }
+
+
+
+// let age = parseInt(prompt('your age'))
+
+// if (age < 12){
+//     alert('that will be 5$ pls')
+// }
+// else if (age < 18){
+//     alert('that will be 10$ pls')
+// }
+// else if (age < 60){
+//     alert('that will be 20$ pls')
+// }
+// else if (age <= 60){
+//     alert('that will be 15$ pls')
+// }
+// else{
+//     alert("input a valid number")
+// }
+
+
+// let time = parseInt(prompt('enter time in 24h format'))
+// if (time < 12){
+//     alert('good morning')
+// }
+// else if (time < 18){
+//     alert('good afternoon')
+// }
+// else {
+//     alert('good evening')
+// }
 
