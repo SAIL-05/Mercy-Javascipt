c8836e76e784a1ff86b58731ef96ed9b31c2787d
Gorgeous568 <iregbeyenm@gmail.com>
2025-05-19 16:31:34 +0100

    new update

--- script.js
+++ script.js
@@ -277,7 +277,7 @@
  
 
 
-// let amount = parseInt(prompt("amount"))
+// let amount = parseInt(prompt(" amount"))
 // if (amount >= 100){
 //     alert (`${amount}` * 0.2)
 // }
@@ -329,4 +329,119 @@
 // else {
 //     alert('good evening')
 // }
+
+
+// // String Method
+
+// let myStr = "Hello Javvascript World"
+// let myName = "Mercy Emmanuel"
+
+// console.log(myStr);
+// console.log(myStr.toUpperCase());
+// console.log(myStr.toLowerCase());
+// console.log(myStr.charAt());
+// console.log(myStr.indexOf("Javascript"));
+// console.log(myStr.includes("Hello"));
+// console.log(myStr.slice(2, 8));
+// console.log(myStr.trim());
+// console.log(myStr.replace("World","Universe"));
+// console.log(myStr.split("o"));
+// console.log(myStr.startsWith("t"));
+// console.log(myStr.endsWith("d"));
+// console.log(myStr.concat)
+// console.log(myName + myStr);
+
+// Number Method
+
+// let num = 42.678;
+// let strNum = "89.56"
+
+// console.log(num);
+// console.log(num.toString())
+// console.log(num.toFixed(2))
+// console.log(parseInt(strNum))
+// console.log(parseFloat(strNum));
+// // console.log(isNaN("trtr"));
+
+// Math Method
+
+// console.log(Math.PI);
+// console.log(Math.round(41.673));
+// console.log(Math.floor(42.9));
+// console.log(Math.ceil(42.1));
+// console.log(Math.max(12, 6, 7))
+// console.log(Math.min(34, 5,7))
+// console.log(Math.pow(2, 10))
+
+// console.log(Math.random() * 10000)
+// console.log(Math.floor(Math.random(Math.random))
+
+
+
+
+// console.log(Math.sqrt(144))
+
+// objects
+
+
+// let student = {
+//     name: "Mercy",
+//     age: 34,
+//     email: "mercy@gmail.com"
+// };
+
+// console.log(student)
+// console.log(student.name)
+// console.log(Object.keys)
+
+// Functions
+
+// function myName
+
+
+
+// Write a function that returns the largest
+
+// write a function that divides two numbers
+
+// let division = () => {
+//     let valueOne = parseInt(prompt("Enter your first Number"));
+// let valueTwo = prompt ("Enter your second Number");
+// let ans = valueOne / valueTwo;
+// console.log(ans);
+// }
+// division()
+
+//  Write a function that checks if a number is even or odd.
+
+
+
+// function num() {
+//     let myNum =parseInt(prompt('enter a number'));
+//     if (myNum % 2 === 0) {
+//     alert(`${myNum} is an even number`);
+// } else{
+//  alert(`${myNum} is an odd number`);
+// }
+// }
+// num()
+    
+
+
+// function highestNo() {
+//     let num1 = prompt ("enter number");
+//     let num2 = prompt ("enter number");
+//     let num3 = prompt ("enter number");
+//     console.log(Math.max(num1,num2,num3)
+// } highestNo()
+
+// Write a function that reverse a String
+
+// function reverse() {
+//     let back = prompt ("enter a name")
+//     console.log(back.split('').reverse('').join(''));
+
+// } reverse()
+
+// Write a function that returns the longest word in a sentence
 
